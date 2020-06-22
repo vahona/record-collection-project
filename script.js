6e642797b8fa9a04b3e3066f903ba4789149ff6f
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-06-22 15:26:49 +0300

    Giving all possible case

--- script.js
+++ script.js
@@ -8,15 +8,46 @@ const recordOfJournal =[{title:"President annoucement", artistname:"Vangue", num
 {title:"Relation", artistname:"Deborah", numbertruck:2, url:"//https://"},] 
 
 // This will show all of the possible action.
-alert("These will be the possible structure:\nTitle\nArtistname\nNumbertruck\nURL")
+alert("These will be the possible structure of the record:\nTitle\nArtistname\nNumbertruck\nURL")
 
 // Here user can choose which action action do they want.
 
-const actionPossible = `Possible choic
-a: Show all the record
-b: Add a new record
-c: Update a specific recored
-d: Remove an existing record
-e: quite the program`
+const actionPossible = `Possible choic :
+a : Show all the record
+b : Add a new record
+c : Update a specific recored
+d : Remove an existing record
+e : quite the program`
 
 let actionChoice = prompt(actionPossible);
+
+// Function for execute the strructure 
+
+const pageForAllowChoic = () => {
+  for(let i = 0; i < recordOfJournal.length; i++){
+  let recordToBeSeen = `Title : ${recordOfJournal[i].title}
+  Artistname : ${recordOfJournal[i].artistname}
+  Numbertrack : ${recordOfJournal[i].numbertruck}
+  URL : ${recordOfJournal[i].recordOfJournal}`;
+
+  alert(recordToBeSeen);
+  }
+}
+switch (actionChoice) {
+    case a:
+
+    break;
+    case b:
+
+    break;
+    case c:
+
+    break;
+    case d:
+
+    break;
+    case e:
+
+    break;
+    
+}
