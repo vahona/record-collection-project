52f75ff2526a7452c59419c607c73fb97ca99afd
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-06-22 15:03:44 +0300

    Created a structur for user

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("try")
+
+// This is the list of record and structure.
+const recordOfJournal =[{title:"President annoucement", artistname:"Vangue", numbertruck:3, url:"//https://"},
+{title:"Traffic", artistname:"Terrry", numbertruck:4, url:"//https://"},
+{title:"Pollution", artistname:"Mathio", numbertruck:5, url:"//https://"},
+{title:"Abuse", artistname:"Peter", numbertruck:7, url:"//https://"},
+{title:"Relation", artistname:"Deborah", numbertruck:2, url:"//https://"},] 
+
+// This will show all of the possible action.
+alert("These will be the possible structure:\nTitle\nArtistname\nNumbertruck\nURL")
+
+// Here user can choose which action action do they want.
+
+const actionPossible = `Possible choic
+a: Show all the record
+b: Add a new record
+c: Update a specific recored
+d: Remove an existing record
+e: quite the program`
+
+let actionChoice = prompt(actionPossible);
